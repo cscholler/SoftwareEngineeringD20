672669807513df55b9c7cdffcbcbeb533436a621
CJACOBSON32 <34342644+CJACOBSON32@users.noreply.github.com>
2020-04-04 23:38:54 -0400

    Turned Dijkstra's algorithm into A*

--- src/main/java/edu/wpi/leviathans/pathFinding/MapParser.java
+++ src/main/java/edu/wpi/leviathans/pathFinding/MapParser.java
@@ -1,61 +1,62 @@
 package edu.wpi.leviathans.pathFinding;
 
 import edu.wpi.leviathans.pathFinding.graph.*;
+
 import java.io.*;
 
 public class MapParser {
 
-  public static String X_LABEL = "x";
-  public static String Y_LABEL = "y";
+    public static String X_LABEL = "x";
+    public static String Y_LABEL = "y";
 
-  public static Graph parseMapToGraph(String nodesPath, String edgesPath) {
-    try {
-      BufferedReader nodeReader = new BufferedReader(new FileReader(nodesPath));
-      BufferedReader edgeReader = new BufferedReader(new FileReader(edgesPath));
+    public static Graph parseMapToGraph(String nodesPath, String edgesPath) {
+        try {
+            BufferedReader nodeReader = new BufferedReader(new FileReader(nodesPath));
+            BufferedReader edgeReader = new BufferedReader(new FileReader(edgesPath));
 
-      // Skip to the second row, the first row is just the labels for the data fields
-      nodeReader.readLine();
-      edgeReader.readLine();
+            // Skip to the second row, the first row is just the labels for the data fields
+            nodeReader.readLine();
+            edgeReader.readLine();
 
-      Graph newGraph = new Graph();
+            Graph newGraph = new Graph();
 
-      String row = "";
-      while ((row = nodeReader.readLine()) != null) {
-        String[] data = row.split(",");
+            String row = "";
+            while ((row = nodeReader.readLine()) != null) {
+                String[] data = row.split(",");
 
-        Node newNode = new Node(data[0]);
-        newNode.data.put(X_LABEL, Integer.parseInt(data[1]));
-        newNode.data.put(Y_LABEL, Integer.parseInt(data[2]));
+                Node newNode = new Node(data[0]);
+                newNode.data.put(X_LABEL, Integer.parseInt(data[1]));
+                newNode.data.put(Y_LABEL, Integer.parseInt(data[2]));
 
-        newGraph.addNode(newNode);
-      }
+                newGraph.addNode(newNode);
+            }
 
-      while ((row = edgeReader.readLine()) != null) {
-        String[] data = row.split(",");
+            while ((row = edgeReader.readLine()) != null) {
+                String[] data = row.split(",");
 
-        Node source = newGraph.getNode(data[1]);
-        Node destination = newGraph.getNode(data[2]);
+                Node source = newGraph.getNode(data[1]);
+                Node destination = newGraph.getNode(data[2]);
 
-        if (source != null && destination != null) {
-          int x1 = (int) source.data.get(X_LABEL);
-          int y1 = (int) source.data.get(Y_LABEL);
-          int x2 = (int) destination.data.get(X_LABEL);
-          int y2 = (int) destination.data.get(Y_LABEL);
+                if (source != null && destination != null) {
+                    int x1 = (int) source.data.get(X_LABEL);
+                    int y1 = (int) source.data.get(Y_LABEL);
+                    int x2 = (int) destination.data.get(X_LABEL);
+                    int y2 = (int) destination.data.get(Y_LABEL);
 
-          int length = (int) Math.round(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+                    int length = (int) Math.round(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
 
-          source.addEdgeTwoWay(new Edge(destination, length));
+                    source.addEdgeTwoWay(new Edge(destination, length));
+                }
+            }
+
+            return newGraph;
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-      }
 
-      return newGraph;
-
-    } catch (FileNotFoundException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
+        return null;
     }
-
-    return null;
-  }
 }
--- src/main/java/edu/wpi/leviathans/pathFinding/PathFinder.java
+++ src/main/java/edu/wpi/leviathans/pathFinding/PathFinder.java
@@ -1,109 +1,145 @@
 package edu.wpi.leviathans.pathFinding;
 
 import edu.wpi.leviathans.pathFinding.graph.*;
+
 import java.util.*;
 
 public class PathFinder {
 
-  class NodeEntry {
-    public Node node;
-    public Node parent;
-    public int shortestPath = Integer.MAX_VALUE;
-    public int distFromDest = 0;
+    class NodeEntry {
+        public Node node;
+        public Node parent;
+        public int shortestPath = Integer.MAX_VALUE;
+        public int distFromDest = 0;
 
-    public NodeEntry(Node p_node) {
-      node = p_node;
+        public NodeEntry(Node p_node) {
+            node = p_node;
+        }
+
+        public NodeEntry(Node p_node, int p_shortestPath) {
+            node = p_node;
+            shortestPath = p_shortestPath;
+        }
+
+        public int absoluteDist() {
+            if (Integer.MAX_VALUE - shortestPath < distFromDest) return Integer.MAX_VALUE;
+
+            return shortestPath + distFromDest;
+        }
     }
 
-    public NodeEntry(Node p_node, int p_shortestPath) {
-      node = p_node;
-      shortestPath = p_shortestPath;
+    private Comparator<NodeEntry> NodeEntrySorter = Comparator.comparing(NodeEntry::absoluteDist);
+    private PriorityQueue<NodeEntry> priorityQueue = new PriorityQueue<NodeEntry>(NodeEntrySorter);
+    private Map<Node, NodeEntry> priorityQueueKey = new HashMap<Node, NodeEntry>();
+
+    private boolean hasCoords = false;
+
+    /**
+     * Uses the A-Star algorithm to get a path between the source and destination node. Returns null if there is no path
+     *
+     * @param source      The Node to start with
+     * @param destination The Node to pathfind to
+     * @return a list of Nodes in representing the path between source and destination (inclusive).
+     */
+    public static LinkedList<Node> aStarPathFind(Graph graph, Node source, Node destination) {
+        PathFinder pathFinder = new PathFinder();
+        return pathFinder.doAStarPathFind(graph, source, destination);
     }
 
-    public int absoluteDist() {
-      if (shortestPath == Integer.MAX_VALUE) return shortestPath;
+    /**
+     * Uses the A-Star algorithm to get a path between the source and destination node. Returns null if there is no path
+     *
+     * @param source      The Node to start with
+     * @param destination The Node to pathfind to
+     * @return a list of Nodes in representing the path between source and destination (inclusive).
+     */
+    public LinkedList<Node> doAStarPathFind(Graph graph, Node source, Node destination) {
 
-      return shortestPath + distFromDest;
+        // Initialize all nodes in the priority queue to infinity but don't add the source
+        for (Node node : graph.getNodes()) {
+            addNode(node);
+        }
+
+        priorityQueueKey.get(source).shortestPath = 0;
+
+        // Calculate the distance of each node from the destination if coordinates are in the data
+        if(hasCoords) {
+            for (NodeEntry entry : priorityQueue) {
+                int x1 = (int) entry.node.data.get(MapParser.X_LABEL);
+                int y1 = (int) entry.node.data.get(MapParser.Y_LABEL);
+                int x2 = (int) destination.data.get(MapParser.X_LABEL);
+                int y2 = (int) destination.data.get(MapParser.Y_LABEL);
+
+                int distance = (int) Math.round(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+
+                entry.distFromDest = distance;
+            }
+        }
+
+        // Start from the source Node and recursively update the priority Queue
+        NodeEntry destNode = aStarPathFindHelper(source, destination);
+
+        return entryToList(destNode);
     }
-  }
 
-  private Comparator<NodeEntry> NodeEntrySorter = Comparator.comparing(NodeEntry::absoluteDist);
-  private PriorityQueue<NodeEntry> priorityQueue = new PriorityQueue<NodeEntry>(NodeEntrySorter);
-  private HashMap<Node, NodeEntry> priorityQueueKey = new HashMap<Node, NodeEntry>();
+    private NodeEntry aStarPathFindHelper(Node source, Node destination) {
 
-  /**
-   * Uses the A-Star algorithm to get a path between the source and destination node. Throws error
-   * if there is no path.
-   *
-   * @param source The Node to start with
-   * @param destination The Node to pathfind to
-   * @return a list of Nodes in representing the path between source and destination (inclusive).
-   */
-  public LinkedList<Node> aStarPathFind(Graph graph, Node source, Node destination) {
-    // TODO: Implement
+        while (!priorityQueue.isEmpty()) {
+            NodeEntry currentNode = priorityQueue.poll();
 
-    // Initialize all nodes in the priority queue to infinity but don't add the source
-    for (Node node : graph.getNodes()) {
-      if (!node.equals(source)) {
+            if (currentNode.node.equals(destination)) {
+                return currentNode;
+            }
+
+            for (Edge edge : currentNode.node.getEdges()) {
+                Node otherNode = edge.destination;
+
+                // Set otherNode NodeEntry shortestPath to currentNode.shortestPath + edge.length
+                NodeEntry otherNodeEntry = priorityQueueKey.get(otherNode);
+                if (currentNode.shortestPath + edge.length < otherNodeEntry.shortestPath) {
+                    otherNodeEntry.shortestPath = currentNode.shortestPath + edge.length;
+                    otherNodeEntry.parent = currentNode.node;
+                }
+            }
+        }
+
+        // Return null if there is no path between source and destination.
+        return null;
+    }
+
+    /**
+     * Converts a NodeEntry's chain of parents into a list representing the path
+     *
+     * @param entry
+     * @return
+     */
+    private LinkedList<Node> entryToList(NodeEntry entry) {
+        LinkedList<Node> path = new LinkedList<Node>();
+        NodeEntry current = entry;
+        while (current.shortestPath != 0) {
+            path.addFirst(current.node);
+            System.out.println(current.node.getName());
+            current = priorityQueueKey.get(current.parent);
+        }
+        System.out.println(current.node.getName());
+        path.addFirst(current.node);
+        return path;
+    }
+
+    private void addNode(Node node) {
         NodeEntry newEntry = new NodeEntry(node);
         priorityQueue.add(newEntry);
         priorityQueueKey.put(node, newEntry);
-      }
-    }
 
-    NodeEntry sourceEntry = new NodeEntry(source, 0);
-
-    priorityQueue.add(sourceEntry);
-    priorityQueueKey.put(source, sourceEntry);
-
-    // Start from the source Node and recursively update the priority Queue
-    NodeEntry destNode = aStarPathFindHelper(source, destination);
-
-    return entryToList(destNode);
-  }
-
-  private NodeEntry aStarPathFindHelper(Node source, Node destination) {
-    // TODO: Implement
-    while (!priorityQueue.isEmpty()) {
-      NodeEntry currentNode = priorityQueue.poll();
-
-      if (currentNode.node.equals(destination)) {
-        return currentNode;
-      }
-
-      for (Edge edge : currentNode.node.getEdges()) {
-        Node otherNode = edge.destination;
-
-        // Set otherNode NodeEntry shortestPath to currentNode.shortestPath + edge.length
-        NodeEntry otherNodeEntry = priorityQueueKey.get(otherNode);
-        if (currentNode.shortestPath + edge.length < otherNodeEntry.shortestPath) {
-          otherNodeEntry.shortestPath = currentNode.shortestPath + edge.length;
-          otherNodeEntry.parent = currentNode.node;
+        if(node.data.containsKey(MapParser.X_LABEL) && node.data.containsKey(MapParser.X_LABEL)) {
+            if(priorityQueue.size() == 1) hasCoords = true;
+        } else {
+            hasCoords = false;
         }
-      }
     }
 
-    // Return null if there is no path between source and destination.
-    return null;
-  }
-
-  /**
-   * Converts a NodeEntry's chain of parents into a list representing the path
-   *
-   * @param entry
-   * @return
-   */
-  private LinkedList<Node> entryToList(NodeEntry entry) {
-    // TODO: Impliment
-    LinkedList<Node> path = new LinkedList<Node>();
-    NodeEntry current = entry;
-    while (current.shortestPath != 0) {
-      path.addFirst(current.node);
-      System.out.println(current.node.getName());
-      current = priorityQueueKey.get(current.parent);
+    private void removeNode(Node node) {
+        priorityQueue.remove(priorityQueueKey.get(node));
+        priorityQueueKey.remove(node);
     }
-    System.out.println(current.node.getName());
-    path.addFirst(current.node);
-    return path;
-  }
 }
