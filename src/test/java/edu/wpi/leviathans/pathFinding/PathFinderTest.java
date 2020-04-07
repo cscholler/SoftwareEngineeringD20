e1a63246992e10b91b2f3c4ab9237f62fa356bda
cctulig <cctulig@gmail.com>
2020-04-07 08:48:02 -0400

    More complicated graph + simple GUI.

--- src/test/java/edu/wpi/leviathans/pathFinding/PathFinderTest.java
+++ src/test/java/edu/wpi/leviathans/pathFinding/PathFinderTest.java
@@ -9,30 +9,45 @@ import edu.wpi.leviathans.pathFinding.graph.Node;
 import org.junit.jupiter.api.Test;
 
 class PathFinderTest {
-    @Test
-    public void createGraph() throws Exception {
-        PathFinder pathfinder = new PathFinder();
-        Graph newGraph = new Graph();
-        Node n1 = new Node("one");
-        Node n2 = new Node("two");
-        Node n3 = new Node("three");
-        Node n4 = new Node("four");
+  @Test
+  public void createGraph() throws Exception {
+    PathFinder pathfinder = new PathFinder();
+    Graph newGraph = new Graph();
+    Node n1 = new Node("n1");
+    Node n2 = new Node("n2");
+    Node n3 = new Node("n3");
+    Node n4 = new Node("n4");
+    Node n5 = new Node("n5");
+    Node n6 = new Node("n6");
+    Node n7 = new Node("n7");
+    Node n8 = new Node("n8");
+    Node n9 = new Node("n9");
 
-        newGraph.addNode(n1);
-        newGraph.addNode(n2);
-        newGraph.addNode(n3);
-        newGraph.addNode(n4);
+    newGraph.addNode(n1);
+    newGraph.addNode(n2);
+    newGraph.addNode(n3);
+    newGraph.addNode(n4);
+    newGraph.addNode(n5);
+    newGraph.addNode(n6);
+    newGraph.addNode(n7);
+    newGraph.addNode(n8);
+    newGraph.addNode(n9);
 
-        n1.addEdgeTwoWay(new Edge(n2, 3));
-        n2.addEdgeTwoWay(new Edge(n3, 5));
-        n3.addEdgeTwoWay(new Edge(n4, 2));
-        n4.addEdgeTwoWay(new Edge(n1, 7));
-        n1.addEdgeTwoWay(new Edge(n3, 348));
-        n2.addEdgeTwoWay(new Edge(n4, 6));
+    n1.addEdgeTwoWay(new Edge(n8, 3));
+    n1.addEdge(new Edge(n9, 5));
+    n2.addEdgeTwoWay(new Edge(n9, 4));
+    n3.addEdge(new Edge(n2, 3));
+    n3.addEdgeTwoWay(new Edge(n4, 2));
+    n4.addEdgeTwoWay(new Edge(n9, 5));
+    n4.addEdge(new Edge(n5, 4));
+    n5.addEdgeTwoWay(new Edge(n6, 2));
+    n6.addEdge(new Edge(n9, 6));
+    n6.addEdgeTwoWay(new Edge(n7, 1));
+    n7.addEdge(new Edge(n8, 3));
 
-        assertEquals(1, 1);
-        // assertEquals(newGraph.getNode("one").getName(), "one");
+    assertEquals(1, 1);
+    // assertEquals(newGraph.getNode("one").getName(), "one");
 
-        assertEquals(3, pathfinder.aStarPathFind(newGraph, n1, n3).size());
-    }
+    assertEquals(12, pathfinder.aStarPathFind(newGraph, n1, n3).recalculateLength());
+  }
 }
