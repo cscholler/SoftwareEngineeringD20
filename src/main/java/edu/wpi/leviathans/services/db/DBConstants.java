d80c0306ab380af2cd3ed03b10d438be6e366aef
Luke Bodwell <ljbodwell@wpi.edu>
2020-04-07 21:46:30 -0400

    Add all museum and painting entries.

--- src/main/java/edu/wpi/leviathans/services/db/DBConstants.java
+++ src/main/java/edu/wpi/leviathans/services/db/DBConstants.java
@@ -1,48 +1,41 @@
 package edu.wpi.leviathans.services.db;
 
 public class DBConstants {
-	// Create museums table in test database
-	public static final String createMuseumsTable =
-			"CREATE TABLE Museums( "
-					+ "id INT NOT NULL GENERATED ALWAYS AS IDENTITY, "
-					+ "name VARCHAR(64), "
-					+ "location VARCHAR(64), "
-					+ "phone_number VARCHAR(16), "
-					+ "date_established DATE, "
-					+ "PRIMARY KEY (id))";
+  // Create museums table in test database
+  public static final String createMuseumsTable =
+      "CREATE TABLE Museums( "
+          + "id INT NOT NULL GENERATED ALWAYS AS IDENTITY, "
+          + "name VARCHAR(64), "
+          + "location VARCHAR(64), "
+          + "phone_number VARCHAR(24), "
+          + "year_established VARCHAR(4), "
+          + "PRIMARY KEY (id))";
 
-	// Create paintings table in test database
-	public static final String createPaintingsTable =
-			"CREATE TABLE Paintings( "
-					+ "id INT NOT NULL GENERATED ALWAYS AS IDENTITY, "
-					+ "name VARCHAR(64), "
-					+ "museum_id INT REFERENCES Museums(id), "
-					+ "artist VARCHAR(64), "
-					+ "date_painted DATE, "
-					+ "PRIMARY KEY (id))";
+  // Create paintings table in test database
+  public static final String createPaintingsTable =
+      "CREATE TABLE Paintings( "
+          + "id INT NOT NULL GENERATED ALWAYS AS IDENTITY, "
+          + "name VARCHAR(64), "
+          + "museum_name VARCHAR, "
+          + "artist VARCHAR(64), "
+          + "year_painted VARCHAR(4), "
+          + "PRIMARY KEY (id))";
 
-	// Add new museum to test database
-	public static final String addMuseum =
-			"INSERT INTO Museums(name, location, phone_number, date_established)"
-					+ "VALUES(?, ?, ?, ?)";
+  // Add new museum to test database
+  public static final String addMuseum =
+      "INSERT INTO Museums(name, location, phone_number, year_established)" + "VALUES(?, ?, ?, ?)";
 
-	// Add new painting to test database
-	public static final String addPainting =
-			"INSERT INTO Paintings(name, museum_id, artist, date_painted)"
-					+ "VALUES(?, ?, ?, ?)";
+  // Add new painting to test database
+  public static final String addPainting =
+      "INSERT INTO Paintings(name, museum_name, artist, year_painted)" + "VALUES(?, ?, ?, ?)";
 
-	// Select all museums in the table
-	public static final String selectAllMuseums =
-			"SELECT *"
-					+ "FROM Museums";
+  // Select all museums in the table
+  public static final String selectAllMuseums = "SELECT *" + "FROM Museums";
 
-	// Select all paintings in the table
-	public static final String selectAllPaintings =
-			"SELECT * "
-					+ "FROM Paintings";
+  // Select all paintings in the table
+  public static final String selectAllPaintings = "SELECT * " + "FROM Paintings";
 
-	// Update the phone number of a given museum
-	public static final String updateMuseumPhone =
-			"INSERT INTO Museums(phone_number)" + "VALUES(?)"
-					+ "WHERE name = ?";
+  // Upyear the phone number of a given museum
+  public static final String updateMuseumPhone =
+      "INSERT INTO Museums(phone_number)" + "VALUES(?)" + "WHERE name = ?";
 }
--- src/main/java/edu/wpi/leviathans/services/db/DatabaseService.java
+++ src/main/java/edu/wpi/leviathans/services/db/DatabaseService.java
@@ -40,16 +40,211 @@ public class DatabaseService {
 	}
 
 	public void buildTestDB() {
+		Statement stmt;
 		String query;
-		ResultSet rs;
 		try {
-			Statement stmt = connection.createStatement();
+			stmt = connection.createStatement();
 			query = DBConstants.createMuseumsTable;
 			stmt.execute(query);
 			query = DBConstants.createPaintingsTable;
 			stmt.execute(query);
 		} catch (SQLException ex) {
 			ex.printStackTrace();
+		}
+		addMuseums();
+		addPaintings();
+	}
+
+	public void addMuseums() {
+		PreparedStatement pStmt;
+		String query;
+		query = DBConstants.addMuseum;
+		try {
+			// Museum 1
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Philadelphia Art Museum");
+			pStmt.setString(2, "Philadelphia, PA");
+			pStmt.setString(3, "215-763-8100");
+			pStmt.setString(4, "1877");
+
+			// Museum 2
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Lourvre Museum");
+			pStmt.setString(2, "Paris, France");
+			pStmt.setString(3, "33 (0)1 40 20 53 17");
+			pStmt.setString(4, "1793");
+
+			// Museum 3
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Museum of Modern Art");
+			pStmt.setString(2, "Midtown Manhattan, New York City");
+			pStmt.setString(3, "212-708-9400");
+			pStmt.setString(4, "1929");
+
+			// Museum 4
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Musée d’Orsay");
+			pStmt.setString(2, "Philadelphia, PA");
+			pStmt.setString(3, "33-(0)14-049-4814");
+			pStmt.setString(4, "1986");
+
+			// Museum 5
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "The Metropolitan Museum of Art");
+			pStmt.setString(2, "New York, NY");
+			pStmt.setString(3, "212-535-7710");
+			pStmt.setString(4, "1870");
+		} catch (SQLException ex) {
+			log.error("SQLException", ex);
+		}
+	}
+
+	public void addPaintings() {
+		PreparedStatement pStmt;
+		String query;
+		query = DBConstants.addPainting;
+		try {
+			// Painting 1
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "The Large bathers");
+			pStmt.setString(2, "Philadelphia Art Museum");
+			pStmt.setString(3, "Paul Cezanne");
+			pStmt.setString(4, "1898");
+
+			// Painting 2
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Nude Descending a Staircase, No. 2");
+			pStmt.setString(2, "Philadelphia Art Museum");
+			pStmt.setString(3, "Marcel Duchamp");
+			pStmt.setString(4, "1912");
+
+			// Painting 3
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "The Gross Clinic");
+			pStmt.setString(2, "Philadelphia Art Museum");
+			pStmt.setString(3, "Thomas Eakins");
+			pStmt.setString(4, "1875");
+
+			// Painting 4
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "The Bride Stripping by Her Bachelors");
+			pStmt.setString(2, "Philadelphia Art Museum");
+			pStmt.setString(3, "Marcel Duchamp");
+			pStmt.setString(4, "1923");
+
+			// Painting 5
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "The Stary Night");
+			pStmt.setString(2, "Museum of Modern Art");
+			pStmt.setString(3, "Vincent Van Gogh");
+			pStmt.setString(4, "1889");
+
+			// Painting 6
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "The Persistence of Memory");
+			pStmt.setString(2, "Museum of Modern Art");
+			pStmt.setString(3, "Salvador Dali");
+			pStmt.setString(4, "1931");
+
+			// Painting 7
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Les Demoiselles d'Avignon");
+			pStmt.setString(2, "Museum of Modern Art");
+			pStmt.setString(3, "Pablo Picaso");
+			pStmt.setString(4, "1907");
+
+			// Painting 8
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Christian's World");
+			pStmt.setString(2, "Museum of Modern Art");
+			pStmt.setString(3, "Andrew Wyeth");
+			pStmt.setString(4, "1948");
+
+			// Painting 9
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Mona Lisa");
+			pStmt.setString(2, "Louvre Museum");
+			pStmt.setString(3, "Leonardo de Vinci");
+			pStmt.setString(4, "1503");
+
+			// Painting 10
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Venud De Milo");
+			pStmt.setString(2, "Louvre Museum");
+			pStmt.setString(3, "Alexandros of Antioch");
+			pStmt.setString(4, "1820");
+
+			// Painting 11
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Liberty Leading the People");
+			pStmt.setString(2, "Louvre Museum");
+			pStmt.setString(3, "Eugene Delacroix");
+			pStmt.setString(4, "1830");
+
+			// Painting 12
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "The Raft of the Medusa");
+			pStmt.setString(2, "Louvre Museum");
+			pStmt.setString(3, "Theodore Gericault");
+			pStmt.setString(4, "1819");
+
+			// Painting 13
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Portrait of Madame X");
+			pStmt.setString(2, "The Metropolitan Museum of Art");
+			pStmt.setString(3, "John Singer Sargent");
+			pStmt.setString(4, "1884");
+
+			// Painting 14
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Study of a Young Woman");
+			pStmt.setString(2, "The Metropolitan Museum of Art");
+			pStmt.setString(3, "Johanna Vermeer");
+			pStmt.setString(4, "1667");
+
+			// Painting 15
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Self-Portrait with a Straw Hat");
+			pStmt.setString(2, "The Metropolitan Museum of Art");
+			pStmt.setString(3, "Vincent van Gogh");
+			pStmt.setString(4, "1887");
+
+			// Painting 16
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Cypresses");
+			pStmt.setString(2, "The Metropolitan Museum of Art");
+			pStmt.setString(3, "Vincent van Gogh");
+			pStmt.setString(4, "1889");
+
+			// Painting 17
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Van Gogh self-Portrait");
+			pStmt.setString(2, "Musée d’Orsay");
+			pStmt.setString(3, "Vincent van Gogh");
+			pStmt.setString(4, "1889");
+
+			// Painting 18
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "L'Origine du monde");
+			pStmt.setString(2, "Musée d’Orsay");
+			pStmt.setString(3, "Gustave Courbet");
+			pStmt.setString(4, "1866");
+
+			// Painting 19
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "A Meeting");
+			pStmt.setString(2, "Musée d’Orsay");
+			pStmt.setString(3, "Marie Bashkrsteff");
+			pStmt.setString(4, "1884");
+
+			// Painting 20
+			pStmt = connection.prepareStatement(query);
+			pStmt.setString(1, "Le Dejeuner sur l'herbe");
+			pStmt.setString(2, "Musée d’Orsay");
+			pStmt.setString(3, "Edouard Manet");
+			pStmt.setString(4, "1863");
+		} catch (SQLException ex) {
+			log.error("SQLException", ex);
 		}
 	}
 
