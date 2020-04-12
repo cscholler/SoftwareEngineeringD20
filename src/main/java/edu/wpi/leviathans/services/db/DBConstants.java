0e439957b7e5d55757309b4419f8cc7b8109d02e
Colin <cjscholler@wpi.edu>
2020-04-12 18:49:09 -0400

    updates DBConstants for ERD

--- src/main/java/edu/wpi/leviathans/services/db/DBConstants.java
+++ src/main/java/edu/wpi/leviathans/services/db/DBConstants.java
@@ -1,61 +1,137 @@
 package edu.wpi.leviathans.services.db;
 
+import javax.print.StreamPrintServiceFactory;
+
 public class DBConstants {
 	public static final String DB_DRIVER = "org.apache.derby.jdbc.EmbeddedDriver";
 	public static final String DB_URL = "jdbc:derby:myDB;create=true";
 	public static final String SERVICE_NAME = "derby-db-embedded-01";
 
-	// Create museums table in test database
-	public static final String createMuseumsTable =
-			"CREATE TABLE Museums( "
-					+ "id INT NOT NULL GENERATED ALWAYS AS IDENTITY, "
-					+ "name VARCHAR(64), "
-					+ "location VARCHAR(64), "
-					+ "phone_number VARCHAR(24), "
-					+ "year_established VARCHAR(4), "
-					+ "PRIMARY KEY (id))";
+	public static final String createNodeTable =
+			"CREATE TABLE Nodes(" +
+					"id VARCHAR(10), " +
+					"x_pos INT, " +
+					"y_pos INT, " +
+					"floor CHAR(1), " +
+					"building VARCHAR(64), " +
+					"l_name VARCHAR(32), " +
+					"s_name VARCHAR(32), " +
+					"team CHAR(1), " +
+					"visible BOOLEAN" +
+					"PRIMARY KEY (id)))";
 
-	// Create paintings table in test database
-	public static final String createPaintingsTable =
-			"CREATE TABLE Paintings( "
-					+ "id INT NOT NULL GENERATED ALWAYS AS IDENTITY, "
-					+ "name VARCHAR(64), "
-					+ "museum_name VARCHAR(64), "
-					+ "artist VARCHAR(64), "
-					+ "year_painted VARCHAR(4), "
-					+ "PRIMARY KEY (id))";
+	public static final String createEdgeTable =
+			"CREATE TABLE Edges(" +
+					"id VARCHAR(21), " +
+					"start VARCHAR(10) REFERENCES Nodes (id), " +
+					"end VARCHAR(10) REFERENCES Node (id)" +
+					"PRIMARY KEY (id)))";
 
-	// Drop museums table from test database
-	public static final String dropMuseumsTable =
-			"DROP TABLE Museums";
+	public static final String createDoctorTable =
+			"CREATE TABLE Doctors(" +
+					"id INT, " +
+					"f_name VARCHAR(32), " +
+					"l_name VARCHAR(32), " +
+					"email VARCHAR(32), " +
+					"office_id VARCHAR(32) REFERENCES Node (id)" +
+					"PRIMARY KEY (id)))";
 
-	// Drop paintings table from test database
-	public static final String dropPaintingsTable =
-			"DROP TABLE Paintings";
+	public static final String createPatientTable =
+			"CREATE TABLE Patients(" +
+					"id INT, " +
+					"f_name VARCHAR(32), " +
+					"l_name VARCHAR(32), " +
+					"doctor_id INT REFERENCES Doctor (id), " +
+					"room_id VARCHAR(10) REFERENCES Node (id)" +
+					"PRIMARY KEY (id)))";
 
-	// Add new museum to test database
-	public static final String addMuseum =
-			"INSERT INTO Museums(name, location, phone_number, year_established)"
-					+ "VALUES(?, ?, ?, ?)";
+	public static final String createMedication_RequestTable =
+			"CREATE TABLE Medication_Requests(" +
+					"id INT NOT NULL GENERATED ALWAYS AS IDENTITY, " +
+					"doctor_id REFERENCES Doctor (id), " +
+					"patient_id REFERENCES Patient (id), " +
+					"nurse_name VARCHAR(64), " +
+					"dose VARCHAR(64), " +
+					"type VARCHAR(64), " +
+					"notes VARCHAR(512)" +
+					"PRIMARY KEY (id)))";
 
-	// Add new painting to test database
-	public static final String addPainting =
-			"INSERT INTO Paintings(name, museum_name, artist, year_painted)"
-					+ "VALUES(?, ?, ?, ?)";
+	public static final String createUserTable =
+			"CREATE TABLE Users(" +
+					"id INT PRIMARY KEY, " +
+					"username VARCHAR(32), " +
+					"password VARCHAR(32), " +
+					"acct_type CHAR(1), " +
+					"PRIMARY KEY (id))";
 
-	// Select all museums in the table
-	public static final String selectAllMuseums =
-			"SELECT * "
-					+ "FROM Museums";
+	public static final String dropNodesTable =
+			"DROP TABLE Nodes";
 
-	// Select all paintings in the table
-	public static final String selectAllPaintings =
-			"SELECT * "
-					+ "FROM Paintings";
+	public static final String dropEdgesTable =
+			"DROP TABLE Edges";
 
-	// Update the phone number of a given museum
-	public static final String updateMuseumPhone =
-			"UPDATE Museums "
-					+ "SET phone_number = ? "
-					+ "WHERE name = ?";
+	public static final String dropDoctorTable =
+			"DROP TABLE Doctors";
+
+	public static final String dropPatientTable =
+			"DROP TABLE Patients";
+
+	public static final String dropMedication_RequestTable =
+			"DROP TABLE Medication_Requests";
+
+	public static final String dropUserTable =
+			"DROP TABLE Users";
+
+	public static final String addNode =
+			"INSERT INTO Nodes(id, x_pos, y_pos, floor, building, l_name, s_name, team)" +
+					"VALUES(?, ?, ?, ?, ?, ?, ?, ?)";
+
+	public static final String updateVisible =
+			"UPDATE Nodes " +
+					"SET visible = ? " +
+					"WHERE id = ?";
+
+	public static final String addEdge =
+			"INSERT INTO Edges(id, start, end)" +
+					"VALUES(?, ?, ?)";
+
+	public static final String addDoctor =
+			"INSERT INTO Doctors(id, f_name, l_name, email, office_id)" +
+					"VALUES(?, ?, ?, ?, ?)";
+
+	public static final String addPatient =
+			"INSERT INTO Patients(id, f_name, l_name, doctor_id, room_id)" +
+					"VALUES(?, ?, ?, ?, ?)";
+
+	public static final String addMedication_Request =
+			"INSERT INTO Medication_Requests(doctor_id, patient_id, nurse_name, dose, type, notes)" +
+					"VALUES(?, ?, ?, ?, ?, ?)";
+
+	public static final String addUser =
+			"INSERT INTO Users(id, username, password, acct_type)" +
+					"VALUES(?, ?, ?, ?)";
+
+	public static final String selectAllNodes =
+			"SELECT * " +
+					"FROM Nodes";
+
+	public static final String selectAllEdges =
+			"SELECT * " +
+					"FROM Edges";
+
+	public static final String selectAllDoctors =
+			"SELECT * " +
+					"FROM Doctors";
+
+	public static final String selectAllPatients =
+			"SELECT * " +
+					"FROM Patients";
+
+	public static final String selectAllMedication_Requests =
+			"SELECT *" +
+					"FROM Medication_Requests";
+
+	public static final String getSelectAllUsers =
+			"SELECT *" +
+					"FROM Users";
 }
