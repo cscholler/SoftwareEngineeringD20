a92276860f8d911e67cb959fd6ad97e64724bf8b
Joshu.ahoy <jdhoy@wpi.edu>
2020-04-17 18:27:49 -0400

    It does not open the .fxml for some reason

--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/LoginController.java
@@ -1,5 +1,5 @@
 package edu.wpi.cs3733.d20.teamL.views.controllers;
-
+//
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
@@ -7,18 +7,25 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LoginController {
-    @FXML JFXPasswordField passwordText;
-    @FXML JFXTextField usernameText;
-    @FXML JFXButton btnEnter;
-    @FXML JFXButton btnBack;
-    private Scene scene;
-    @FXML AnchorPane body;
+    @FXML
+    JFXPasswordField passwordText;
+    @FXML
+    JFXTextField usernameText;
+    @FXML
+    Button btnEnter;
+    @FXML
+    JFXButton btnBack;
+    @FXML
+    AnchorPane root;
+    //private Scene scene;
+    // @FXML AnchorPane body;
 
 //    private void handleButtonAction(ActionEvent e) throws IOException {
 ////        Stage stage;
@@ -30,30 +37,21 @@ public class LoginController {
 ////        }
 ////    }
 
-//   @FXML
-//    public void initialize() {
-//        scene = body.getScene();
-//    }
 
-@FXML
-private String handleLogin(ActionEvent e) throws IOException {
-        Stage stage;
-        Parent root;
-        if(e.getSource() == btnEnter){
-            String user = usernameText.getText();
-            String password = passwordText.getText();
 
-            if(user == "Doctor" && password == "Doctor"){
-                return "Doctor";
-            } else if(user == "Nurse" && password == "Nurse"){
-                return "Nurse";
-            } else if(user == "Admin" && password == "Admin"){
-                return "Admin";
-            } else{
-                return "Incorrect username or password";
-            }
+    @FXML
+    private String handleLogin(ActionEvent e) throws IOException {
+        String user = usernameText.getText();
+        String password = passwordText.getText();
 
+        if (user == "Doctor" && password == "Doctor") {
+            return "Doctor";
+        } else if (user == "Nurse" && password == "Nurse") {
+            return "Nurse";
+        } else if (user == "Admin" && password == "Admin") {
+            return "Admin";
+        } else {
+            return "Incorrect username or password";
         }
-        return null;
     }
 }
