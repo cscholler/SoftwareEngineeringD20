565ec81f610deee0091da7636ff0b1b580475bb7
Winnie Ly <wsly@wpi.edu>
2020-04-17 20:33:43 -0400

    Added NavigationController

---
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/NavigationController.java
@@ -0,0 +1,13 @@
+package edu.wpi.cs3733.d20.teamL.views.controllers;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
+public class NavigationController {
+
+    public void handleButtonAction(ActionEvent actionEvent) throws IOException {
+
+
+    }
+}
