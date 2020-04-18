cd87f6189558019e556e5e82c4a65764d2b3789b
Luke Bodwell <ljbodwell@wpi.edu>
2020-04-17 21:43:28 -0400

    Update CSV parser. Fix resource loading.

---
+++ src/main/java/edu/wpi/cs3733/d20/teamL/util/io/CSVReader.java
@@ -0,0 +1,14 @@
+package edu.wpi.cs3733.d20.teamL.util.io;
+
+import java.util.ArrayList;
+import org.apache.commons.csv.CSVParser;
+
+public class CSVParser {
+	public ArrayList<ArrayList<String>> readCSVFile() {
+		ArrayList<ArrayList<String>> data = new ArrayList<>();
+
+
+
+		return data;
+	}
+}
