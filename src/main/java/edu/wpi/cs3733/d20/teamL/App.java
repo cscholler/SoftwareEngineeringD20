9e6942355e80b2bf5c20fa6dab51fd389487ff28
Joshu.ahoy <jdhoy@wpi.edu>
2020-04-17 21:21:21 -0400

    Functional psuedo login. Expected to be added to other application

--- src/main/java/edu/wpi/cs3733/d20/teamL/App.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/App.java
@@ -27,7 +27,7 @@ public class App extends Application {
 		Scene scene = new Scene(root);
 
 		stage.setScene(scene);
-		stage.setTitle("Genuine Coder Login");
+		stage.setTitle("Employee/Administrator Login");
 		stage.show();
 	}
 
--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/LoginController.java
@@ -3,22 +3,21 @@ package edu.wpi.cs3733.d20.teamL.views.controllers;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
-/**
- *
- * @author villan
- */
+
 public class LoginController implements Initializable {
 
-
-    @FXML
-    private AnchorPane anchorPane;
 
     @FXML
     private JFXTextField username;
@@ -27,15 +26,37 @@ public class LoginController implements Initializable {
     private JFXPasswordField pass;
 
     @FXML
-    private JFXButton login;
+    private JFXButton login, btnCancel;
+
 
     @FXML
-    void makeLogin(ActionEvent event) {
-        if(username.getText().equals("genuine")&&pass.getText().equals("coder"))
-        {
-            System.out.println("Welcome");
+    private String handleLogin(ActionEvent e) throws IOException {
+        String user = username.getText();
+        String password = pass.getText();
+        String status = "Incorrect username or password";
+
+        if (user.equals("Doctor") && password.equals("Doctor")) {
+            System.out.println("Doctor");
+            status = "Doctor";
+        } else if (user.equals("Nurse") && password.equals("Nurse")) {
+            System.out.println("Nurse");
+            status = "Nurse";
+        } else if (user.equals("Admin") && password.equals("Admin")) {
+            System.out.println("Admin");
+            status = "Admin";
         }
+        System.out.println(status);
+        pass.clear();
+        username.clear();
+
+        return status;
     }
+
+//    private void handleCancel(ActionEvent e) throws IOException {
+//        Stage stage = (Stage) btnCancel.getScene().getWindow();
+//        Parent root = FXMLLoader.load(getClass().getResource("edu/wpi/cs3733/d20/teamL/views/MapViewer.fxml"));
+//        stage.close();
+//    }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
