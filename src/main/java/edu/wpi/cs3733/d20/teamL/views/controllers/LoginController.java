2d0f4013111a6d0f786cf37826bad08e8ae4ba89
Joshu.ahoy <jdhoy@wpi.edu>
2020-04-18 20:07:53 -0400

    Added commentation

--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/LoginController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/LoginController.java
@@ -35,9 +35,13 @@ public class LoginController implements Initializable {
     @FXML
     private Text incorrectText;
 
-
+    /**
+     * Controls the login feature setting
+     * @param e Tracks which button is pressed
+     * @throws IOException
+     */
     @FXML
-    private String handleLogin(ActionEvent e) throws IOException {
+    private void handleLogin(ActionEvent e) throws IOException {
         String user = username.getText();
         String password = pass.getText();
         incorrectText.setVisible(false);
@@ -48,29 +52,30 @@ public class LoginController implements Initializable {
         fadeTransition.setFromValue(0.0);
         fadeTransition.setToValue(1.0);
         fadeTransition.setCycleCount(3);
+        if (e.getSource() == btnCancel) {
+            System.out.println("Cancel");
+        } else if (e.getSource() == login) {
 
+            if (user.equals("Doctor") && password.equals("Doctor")) {
+                System.out.println("Doctor");
+                status = "Doctor";
+            } else if (user.equals("Nurse") && password.equals("Nurse")) {
+                System.out.println("Nurse");
+                status = "Nurse";
+            } else if (user.equals("Admin") && password.equals("Admin")) {
+                System.out.println("Admin");
+                status = "Admin";
+            } else {
+                incorrectText.setVisible(true);
+                fadeTransition.play();
+            }
+            System.out.println(status);
+            pass.clear();
+            username.clear();
 
-        if (user.equals("Doctor") && password.equals("Doctor")) {
-            System.out.println("Doctor");
-            status = "Doctor";
-        } else if (user.equals("Nurse") && password.equals("Nurse")) {
-            System.out.println("Nurse");
-            status = "Nurse";
-        } else if (user.equals("Admin") && password.equals("Admin")) {
-            System.out.println("Admin");
-            status = "Admin";
+            //return status;
         }
-        else {
-            incorrectText.setVisible(true);
-            fadeTransition.play();
-        }
-        System.out.println(status);
-        pass.clear();
-        username.clear();
-
-        return status;
     }
-
 //    private void handleCancel(ActionEvent e) throws IOException {
 //        Stage stage = (Stage) btnCancel.getScene().getWindow();
 //        Parent root = FXMLLoader.load(getClass().getResource("edu/wpi/cs3733/d20/teamL/views/MapViewer.fxml"));
