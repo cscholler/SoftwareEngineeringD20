324d7b2c8fa8605bc2ecb7efa8f2fd81d183ce51
Joshu.ahoy <jdhoy@wpi.edu>
2020-04-20 14:37:37 -0400

    Improved Notifications page

--- src/main/java/edu/wpi/cs3733/d20/teamL/entities/MedicineRequest.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/entities/MedicineRequest.java
@@ -1,18 +1,68 @@
 package edu.wpi.cs3733.d20.teamL.entities;
 
 public class MedicineRequest {
-    public MedicineRequest(String patientName, String nurseName, String medType, String dose, String roomNum) {
+
+    String patientName;
+    String patientID;
+    String nurseName;
+    String medType;
+    String dose;
+    String roomNum;
+
+    public MedicineRequest(String patientName, String patientID, String nurseName, String medType, String dose, String roomNum) {
         this.patientName = patientName;
+        this.patientID = patientID;
         this.nurseName = nurseName;
         this.medType = medType;
         this.dose = dose;
         this.roomNum = roomNum;
     }
 
-    String patientName;
-    String nurseName;
-    String medType;
-    String dose;
-    String roomNum;
+    public String getPatientName() {
+        return patientName;
+    }
 
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public String getPatientID() {
+        return patientID;
+    }
+
+    public void setPatientID(String patientID) {
+        this.patientID = patientID;
+    }
+
+    public String getNurseName() {
+        return nurseName;
+    }
+
+    public void setNurseName(String nurseName) {
+        this.nurseName = nurseName;
+    }
+
+    public String getMedType() {
+        return medType;
+    }
+
+    public void setMedType(String medType) {
+        this.medType = medType;
+    }
+
+    public String getDose() {
+        return dose;
+    }
+
+    public void setDose(String dose) {
+        this.dose = dose;
+    }
+
+    public String getRoomNum() {
+        return roomNum;
+    }
+
+    public void setRoomNum(String roomNum) {
+        this.roomNum = roomNum;
+    }
 }
--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/NotificationsPageController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/NotificationsPageController.java
@@ -2,6 +2,7 @@ package edu.wpi.cs3733.d20.teamL.views.controllers;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
+import edu.wpi.cs3733.d20.teamL.entities.MedicineRequest;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -10,6 +11,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
@@ -24,22 +27,22 @@ public class NotificationsPageController implements Initializable {
     @FXML
     JFXButton btnBack;
     @FXML
-    JFXListView<String> notifications;
+    JFXListView<MedicineRequest> notifications;
 
 
     @FXML
     private void loadData(){
         list.removeAll(list);
-        String test = "Josh Hoy";
-        String test1 = "JHoy";
-        String test2 = "Hoyboy";
+        MedicineRequest test = new MedicineRequest("Josh Hoy", "1234", "Joan", "Aspirin", "2cc", "305");
+        MedicineRequest test1 = new MedicineRequest("Luke Bodwell", "3333", "Joy", "Oxicotin", "20cc", "430");
+        MedicineRequest test2 = new MedicineRequest("Conrad Tulig", "5454", "Jill", "Cough Syrup", "5cc", "100");
         list.addAll(test, test1, test2);
         notifications.getItems().addAll(list);
 
     }
 @FXML
 private void displaySelected(MouseEvent e){
-    String message = notifications.getSelectionModel().getSelectedItem();
+    String message = notifications.getSelectionModel().getSelectedItem().getPatientName();
        if(message == null || message.isEmpty()){
            System.out.println("Nothing");
        }else{
@@ -65,6 +68,17 @@ private void displaySelected(MouseEvent e){
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-     loadData();
+
+        loadData();
+
+        notifications.setCellFactory(param -> new ListCell<MedicineRequest>() {
+                @Override
+                protected void updateItem(MedicineRequest medReq, boolean empty) {
+                    super.updateItem(medReq, empty);
+                    if (medReq != null) {
+                        setText(medReq.getDose() +  " of " + medReq.getMedType() + " for " + medReq.getPatientName());
+                    }
+            }
+        });
     }
 }
