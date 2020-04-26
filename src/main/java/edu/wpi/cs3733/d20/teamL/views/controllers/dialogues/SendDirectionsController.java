4b155d1fc5c7569b614566daaf3991b6f4f77640
Joshu.ahoy <jdhoy@wpi.edu>
2020-04-26 18:18:32 -0400

    UI is updated and better

--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/dialogues/SendDirectionsController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/dialogues/SendDirectionsController.java
@@ -4,9 +4,7 @@ import com.google.inject.Inject;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.d20.teamL.services.IMessengerService;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.MenuButton;
 import javafx.stage.Stage;
 
 public class SendDirectionsController {
@@ -15,57 +13,38 @@ public class SendDirectionsController {
     @FXML
     private JFXTextField emailField;
     @FXML
-    private JFXButton btnCancel, btnEmail, btnText;
-    @FXML
-    private MenuButton carrierSelector;
+    private JFXButton btnCancel;
     @FXML
     private JFXTextField phoneNumberField;
-    @FXML
-    void onSprint(ActionEvent event) {
-        carrierSelector.setText("Sprint");
-    }
-    @FXML
-    void onTMobile(ActionEvent event) {
-        carrierSelector.setText("T-Mobile");
-    }
-    @FXML
-    void onATT(ActionEvent event) {
-        carrierSelector.setText("AT&T");
-    }
-    @FXML
-    void onVerizon(ActionEvent event) {
-    	carrierSelector.setText("Verizon");
-    }
+
 
     /**
      * closes text directions window
-     * @param event tracks when button is clicked
      */
     @FXML
-    void handleButtonCancel(ActionEvent event) {
+    void cancelClicked() {
         Stage stage = (Stage) btnCancel.getScene().getWindow();
         stage.close();
     }
 
     /**
      * sends either a text or an email
-     * @param event tracks which button is clicked
+     *
      */
-	@FXML
-	void handleSend(ActionEvent event) {
-	    //sends text
-		if (event.getSource() == btnText) {
-			String phoneNumber = phoneNumberField.getText();
-			//TODO: replace with Twilio implementation
-			messenger.sendText(messenger.getDirections(), phoneNumber);
 
-		// sends email
-		} else if (event.getSource() == btnEmail) {
-			if (!emailField.getText().equals("")) {
-				//TODO: replace with SendGrid implementation
-				String emailAddress = emailField.getText();
-				messenger.sendEmail(messenger.getDirections(), emailAddress);
-			}
-		}
-	}
+    @FXML
+	void sendText(){
+        String phoneNumber = phoneNumberField.getText();
+        //TODO: replace with Twilio implementation
+        messenger.sendText(messenger.getDirections(), phoneNumber);
+    }
+
+    @FXML
+    void sendEmail(){
+        //TODO: replace with SendGrid implementation
+        String emailAddress = emailField.getText();
+        messenger.sendEmail(messenger.getDirections(), emailAddress);
+    }
+
+
 }
