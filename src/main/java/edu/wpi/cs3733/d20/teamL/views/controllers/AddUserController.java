ad7d09d25d7574b26e23fabf5c78d1b369c80e7f
Joshu.ahoy <jdhoy@wpi.edu>
2020-04-27 22:14:56 -0400

    New add user

--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/AddUserController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/AddUserController.java
@@ -1,6 +1,8 @@
 package edu.wpi.cs3733.d20.teamL.views.controllers;
 
 import com.google.inject.Inject;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.d20.teamL.services.db.DBConstants;
 import edu.wpi.cs3733.d20.teamL.services.db.IDatabaseCache;
@@ -8,10 +10,13 @@ import edu.wpi.cs3733.d20.teamL.services.db.IDatabaseService;
 import edu.wpi.cs3733.d20.teamL.services.db.SQLEntry;
 import edu.wpi.cs3733.d20.teamL.util.FXMLLoaderHelper;
 import edu.wpi.cs3733.d20.teamL.util.io.DBTableFormatter;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuButton;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import lombok.extern.slf4j.Slf4j;
 
@@ -22,22 +27,31 @@ import java.util.ResourceBundle;
 
 @Slf4j
 public class AddUserController implements Initializable {
+    ObservableList<String> serviceOptions = FXCollections.observableArrayList("Security", "Internal Transport", "External Transport", "Sanitation", "Maintenance", "Pharmacist", "Gift Shop", "Interpreter", "Information Technology");
+    ObservableList<String> userOptions = FXCollections.observableArrayList("Doctor", "Nurse", "Admin", "Staff");
 
     DBTableFormatter formatter = new DBTableFormatter();
     private FXMLLoaderHelper loaderHelper = new FXMLLoaderHelper();
 
     @FXML
-    JFXTextField doctorIDText, fNameText, lNameText, servicesText, usernameText, passwordText;
+    JFXTextField doctorIDText, fNameText, lNameText, servicesText, usernameText, passwordText, languages;
     @FXML
     MenuButton role;
     @FXML
     Label lblConfirmation;
+    @FXML
+    private JFXComboBox serviceCombo, userCombo;
+    @FXML
+    private JFXCheckBox securityBox, inTransportBox, exTransportBox, interpreterBox, giftShopBox, pharmacistBox, managerBox, itBox, maintenanceBox, sanitationBox;
+    @FXML
+    private VBox boxOService;
     @Inject
     private IDatabaseService db;
     @Inject
     private IDatabaseCache cache;
 //    @Inject
 //    private ILoginManager loginManager;
+    String user;
 
     @FXML
     private void submitClicked() {
@@ -98,23 +112,54 @@ public class AddUserController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         formatter.reportQueryResults(db.executeQuery(new SQLEntry(DBConstants.SELECT_ALL_DOCTORS)));
 
+        serviceCombo.setItems(serviceOptions);
+        userCombo.setItems(userOptions);
     }
+
     @FXML
-    private void nurseSelected(){
-        role.setText("Nurse");
-        doctorIDText.setVisible(false);
-        doctorIDText.setDisable(true);
+    private void userSelected(){
+        user = (String) userCombo.getValue();
+        if(!(user == "Admin")){
+            managerBox.setVisible(true);
+            managerBox.setDisable(false);
+            if(managerBox.isSelected()){
+                serviceCombo.setVisible(true);
+                serviceCombo.setDisable(false);
+                boxOService.setVisible(false);
+                boxOService.setDisable(true);
+                languages.setVisible(false);
+                languages.setDisable(true);
+            } else {
+                System.out.println("Services");
+                serviceCombo.setVisible(false);
+                serviceCombo.setDisable(true);
+                boxOService.setVisible(true);
+                boxOService.setDisable(false);
+                if(interpreterBox.isSelected()){
+                    languages.setVisible(true);
+                    languages.setDisable(false);
+                } else{
+                    languages.setVisible(false);
+                    languages.setDisable(true);
+                }
+            }
+        } else{
+            managerBox.setDisable(true);
+            managerBox.setVisible(false);
+            languages.setVisible(false);
+            languages.setDisable(true);
+            boxOService.setVisible(false);
+            boxOService.setDisable(true);
+        }
+
+        if(user == "Doctor"){
+            doctorIDText.setVisible(true);
+            doctorIDText.setDisable(false);
+        } else{
+            doctorIDText.setVisible(false);
+            doctorIDText.setDisable(true);
+        }
     }
-    @FXML
-    private void doctorSelected(){
-        role.setText("Doctor");
-        doctorIDText.setVisible(true);
-        doctorIDText.setDisable(false);
-    }
-    @FXML
-    private void adminSelected(){
-        role.setText("Admin");
-        doctorIDText.setVisible(false);
-        doctorIDText.setDisable(true);
-    }
+
+
 }
