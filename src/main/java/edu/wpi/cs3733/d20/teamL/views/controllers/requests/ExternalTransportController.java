ca51b122019cbe825d9cbadbe7de307b4e600171
Joshu.ahoy <jdhoy@wpi.edu>
2020-04-30 21:54:41 -0400

    Better time format

--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/requests/ExternalTransportController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/requests/ExternalTransportController.java
@@ -71,7 +71,7 @@ public class ExternalTransportController implements Initializable {
         String type = (String) transportSelector.getValue();
 
         String status = "0";
-        String dateAndTime = new SimpleDateFormat("MM-dd-yyyy hh:mm:ss").format(new Date());
+        String dateAndTime = new SimpleDateFormat("M-dd-yyyy | h:mm aa").format(new Date());
         int rows = 0;
         if (!(start.isEmpty() || end.isEmpty() || type.isEmpty())) {
             rows = db.executeUpdate(new SQLEntry(DBConstants.ADD_SERVICE_REQUEST, new ArrayList<>(Arrays.asList(null, manager.getCurrentUser().getUsername(), null, start, "external_transportation", type, end, status, dateAndTime))));
--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/requests/ITServiceController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/requests/ITServiceController.java
@@ -104,7 +104,7 @@ public class ITServiceController implements Initializable {
         String notes = notesText.getText();
 
         String status = "0";
-        String dateAndTime = new SimpleDateFormat("MM-dd-yyyy hh:mm:ss").format(new Date());
+        String dateAndTime = new SimpleDateFormat("M-dd-yyyy | h:mm aa").format(new Date());
 
         int rows = db.executeUpdate(new SQLEntry(DBConstants.ADD_SERVICE_REQUEST,
                 new ArrayList<>(Arrays.asList(null, userName, null, searchFields.getNode(location).getID(), "information_technology", type, notes, status, dateAndTime))));
