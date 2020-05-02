e3697f18d4c3f2ef3b945d6920d8d537fdd0c91c
Colin <cjscholler@wpi.edu>
2020-05-01 20:19:52 -0400

    added radio buttons to style sheet

--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/requests/SanitationPaneController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/requests/SanitationPaneController.java
@@ -1,9 +1,26 @@
 package edu.wpi.cs3733.d20.teamL.views.controllers.requests;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXCheckBox;
+import com.jfoenix.controls.JFXTextArea;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.MouseEvent;
 
 import java.io.IOException;
 
 public class SanitationPaneController {
+    @FXML
+    final ToggleGroup priorityGroup = new ToggleGroup();
+    @FXML
+    private JFXTextField incidentLocationText;
+    @FXML
+    private JFXCheckBox bioHazardCheckBox, spillCheckBox;
+    @FXML
+    private JFXTextArea additionalNotesText;
 
+    public void submitServiceRequest(ActionEvent actionEvent) {
+    }
 }
