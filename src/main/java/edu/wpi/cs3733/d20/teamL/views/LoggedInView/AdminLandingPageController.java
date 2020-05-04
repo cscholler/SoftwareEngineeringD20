16ec5556d5a03752f62739fcb37c43e34e0ff69c
vebuyck <vebuyck@sizzlescene.com>
2020-05-03 21:13:05 -0400

    idk wats happening

--- src/main/java/edu/wpi/cs3733/d20/teamL/views/LoggedInView/AdminLandingPageController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/LoggedInView/AdminLandingPageController.java
@@ -1,27 +1,42 @@
 package edu.wpi.cs3733.d20.teamL.views.LoggedInView;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXListView;
+import edu.wpi.cs3733.d20.teamL.services.db.IDatabaseCache;
 import edu.wpi.cs3733.d20.teamL.services.db.IDatabaseService;
 import edu.wpi.cs3733.d20.teamL.util.FXMLLoaderHelper;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.concurrent.Executor;
+import java.util.concurrent.FutureTask;
 
 import edu.wpi.cs3733.d20.teamL.services.users.ILoginManager;
 
 import javax.inject.Inject;
 
 @Slf4j
-public class AdminLandingPageController {
+public class AdminLandingPageController implements Initializable {
     FXMLLoaderHelper loaderHelper = new FXMLLoaderHelper();
     @FXML
     private JFXComboBox<String> selectDatabase;
@@ -36,8 +51,7 @@ public class AdminLandingPageController {
     @Inject
     IDatabaseService db;
 
-
-    //@Override
+    @Override
     public void initialize(URL location, ResourceBundle resources) {
         selectDatabase.setItems(databaseOptions);
     }
@@ -79,4 +93,55 @@ public class AdminLandingPageController {
         db.rebuildDatabase();
     }
 
+
+    @FXML
+    private void addUserClicked() {
+        try {
+            System.out.println("Got here");
+            Parent root = loaderHelper.getFXMLLoader("AddUser").load();
+            loaderHelper.setupScene(new Scene(root));
+        } catch (IOException e) {
+            log.error("Encountered IOException", e);
+        }
+    }
+
+    @FXML
+    private void addDoctorClicked() {
+        try {
+            Parent root = loaderHelper.getFXMLLoader("AddDoctor").load();
+            loaderHelper.setupScene(new Scene(root));
+        } catch (IOException e) {
+            log.error("Encountered IOException", e);
+        }
+    }
+
+    @FXML
+    private void addPatientClicked() {
+        try {
+            Parent root = loaderHelper.getFXMLLoader("AddPatient").load();
+            loaderHelper.setupScene(new Scene(root));
+        } catch (IOException e) {
+            log.error("Encountered IOException", e);
+        }
+    }
+
+    @FXML
+    public void importClicked(ActionEvent actionEvent) {
+        try {
+            Parent root = loaderHelper.getFXMLLoader("dialogues/ImportDialogue").load();
+            loaderHelper.setupPopup(new Stage(), new Scene(root));
+        } catch (IOException ex) {
+            log.error("Encountered IOException", ex);
+        }
+    }
+
+    @FXML
+    public void exportClicked(ActionEvent actionEvent) {
+        try {
+            Parent root = loaderHelper.getFXMLLoader("dialogues/ExportDialogue").load();
+            loaderHelper.setupPopup(new Stage(), new Scene(root));
+        } catch (IOException ex) {
+            log.error("Encountered IOException", ex);
+        }
+    }
 }
--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/dialogues/RebuildDatabasePopupController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/dialogues/RebuildDatabasePopupController.java
@@ -1,15 +1,108 @@
 package edu.wpi.cs3733.d20.teamL.views.controllers.dialogues;
 
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXSpinner;
+import edu.wpi.cs3733.d20.teamL.services.db.IDatabaseService;
+import edu.wpi.cs3733.d20.teamL.services.users.ILoginManager;
+//import edu.wpi.cs3733.d20.teamL.util.AsyncTaskManager;
+//import edu.wpi.cs3733.d20.teamL.util.FXMLLoaderFactory;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.concurrent.*;
+import com.jfoenix.controls.JFXButton;
+import edu.wpi.cs3733.d20.teamL.services.db.IDatabaseService;
+import edu.wpi.cs3733.d20.teamL.services.users.ILoginManager;
+import edu.wpi.cs3733.d20.teamL.util.FXMLLoaderHelper;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import lombok.extern.slf4j.Slf4j;
 
+import javax.inject.Inject;
+import java.util.concurrent.Executor;
+import java.util.concurrent.FutureTask;
+
+@Slf4j
 public class RebuildDatabasePopupController {
 
-    @FXML
-    private JFXButton yesBtn;
+    FXMLLoaderHelper loaderHelper = new FXMLLoaderHelper();
+
+    @Inject
+    ILoginManager login;
+
+    @Inject
+    IDatabaseService db;
+
     @FXML
     private JFXButton noBtn;
 
+    @FXML
+    public void clearClicked() {
+        log.warn("Rebuilding database");
+
+        Executor uiExec = Platform::runLater;
+
+        Alert loading = new Alert(Alert.AlertType.NONE);
+        loading.setResult(ButtonType.OK);
+//        JFXSpinner spinner = new JFXSpinner();
+//        spinner.setRadius(10);
+        ImageView spinner = new ImageView(new Image("edu/wpi/cs3733/d20/teamL/assets/spinner.gif"));
+        spinner.setPreserveRatio(true);
+        spinner.setFitWidth(40);
+        loading.setGraphic(spinner);
+        loading.setContentText("Rebuilding database...");
+
+        // LOOK HOW SMART I AM
+        Button btn = new Button("Start");
+        btn.setOnAction(evt -> {
+            btn.setDisable(true);
+
+            // make alert appear / disappear
+            Thread t = new Thread(() -> {
+                boolean showing = false;
+                while (true) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException ex) {
+                        log.error("{}", ex);
+                    }
+                    Runnable action = showing ? loading::close : loading::show;
+                    Platform.runLater(action);
+                    showing = !showing;
+                }
+            });
+            t.setDaemon(true);
+            t.start();
+        });
+
+        loading.show();
+
+        /*AsyncTaskManager.newTask(() -> {
+            db.rebuildDatabase();
+            log.info("Finished rebuilding database");
+            uiExec.execute(new FutureTask<>(() -> {
+                loading.close();
+                showDone();
+                return null;
+            }));
+        });*/
+    }
+
+    private Boolean showDone() {
+        log.info("showDone() Called");
+        Alert done = new Alert(Alert.AlertType.INFORMATION);
+        done.setContentText("Finished rebuilding database");
+        done.showAndWait();
+
+        return true;
+    }
 
 
 }
