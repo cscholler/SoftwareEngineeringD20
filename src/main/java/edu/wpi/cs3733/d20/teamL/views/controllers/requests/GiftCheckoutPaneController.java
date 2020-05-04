05537e8fb2f7c23d339d7465b08ac68fce53efef
Colin <cjscholler@wpi.edu>
2020-05-04 01:51:45 -0400

    It's just about done, the database just isn't set up for it

--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/requests/GiftCheckoutPaneController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/requests/GiftCheckoutPaneController.java
@@ -1,18 +1,27 @@
 package edu.wpi.cs3733.d20.teamL.views.controllers.requests;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextArea;
 import com.jfoenix.controls.JFXTextField;
+import edu.wpi.cs3733.d20.teamL.services.db.DBConstants;
 import edu.wpi.cs3733.d20.teamL.services.db.IDatabaseCache;
+import edu.wpi.cs3733.d20.teamL.services.db.IDatabaseService;
+import edu.wpi.cs3733.d20.teamL.services.db.SQLEntry;
+import edu.wpi.cs3733.d20.teamL.services.users.ILoginManager;
 import edu.wpi.cs3733.d20.teamL.util.FXMLLoaderHelper;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -22,6 +31,8 @@ public class GiftCheckoutPaneController {
     @Inject
     private IDatabaseCache cache;
     private FXMLLoaderHelper loaderHelper = new FXMLLoaderHelper();
+    private IDatabaseService db;
+    private ILoginManager loginManager;
     @FXML
     private TableView orderTable;
     @FXML
@@ -50,7 +61,15 @@ public class GiftCheckoutPaneController {
     }
 
     public void placeOrder(ActionEvent actionEvent) {
-        System.out.println("Placing Order");
+        String firstName = firstNameText.getText();
+        String lastName = lastNameText.getText();
+        String patientID = db.getTableFromResultSet(db.executeQuery(new SQLEntry(DBConstants.GET_PATIENT_ID, new ArrayList<>(Arrays.asList(firstName, lastName))))).get(0).get(0);
+        String sender = senderText.getText();
+        String deliveryInstructions = additionalNotesText.getText();
+        String specialMessage = specialMessageText.getText();
+
+//        int rows = db.executeUpdate((new SQLEntry(DBConstants.ADD_GIFT_DELIVERY_REQUEST,
+//                new ArrayList<>(Arrays.asList(patientID,sender,loginManager.getCurrentUser().getUsername(),null,)))))
     }
 
     public class GiftDetails {
