c6228bd681763fe170b4b1c4d8389ee00b86978a
Winnie Ly <wsly@wpi.edu>
2020-05-08 22:18:47 -0400

    Added Back functionality

--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/logged_in/AnalyticsController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/logged_in/AnalyticsController.java
@@ -1,25 +1,35 @@
 package edu.wpi.cs3733.d20.teamL.views.controllers.logged_in;
 
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
+import edu.wpi.cs3733.d20.teamL.util.FXMLLoaderFactory;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.XYChart;
+import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 @Slf4j
 public class AnalyticsController implements Initializable {
-    @FXML
-    private JFXComboBox<String> timeBox;
 
     @FXML
+    private JFXComboBox<String> timeBox;
+    @FXML
+    private JFXButton btnCancel;
+    @FXML
     private BarChart<?, ?> ServiceReqHisto;
+
+    private FXMLLoaderFactory loaderFactory = new FXMLLoaderFactory();
 
     ObservableList<String> timeOptions = FXCollections.observableArrayList("Any time", "Past hour", "Past 24 hours", "Past month", "Past year");
 
@@ -44,6 +54,15 @@ public class AnalyticsController implements Initializable {
 
         ServiceReqHisto.getData().addAll(set);
 
+    }
+
+    @FXML
+    void handleCancel() {
+        try {
+            loaderFactory.goBack();
+        } catch (Exception ex) {
+            log.error("Encountered Exception.", ex);
+        }
     }
 
     @FXML
