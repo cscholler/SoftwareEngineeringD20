ba86bcd439013c282df33c5874cb4c96cc8188e7
Winnie Ly <wsly@wpi.edu>
2020-05-08 21:07:22 -0400

    Added basic bar/histo graph

--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/logged_in/AnalyticsController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/logged_in/AnalyticsController.java
@@ -1,4 +1,50 @@
 package edu.wpi.cs3733.d20.teamL.views.controllers.logged_in;
 
-public class AnalyticsController {
+
+import com.jfoenix.controls.JFXComboBox;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.XYChart;
+import lombok.extern.slf4j.Slf4j;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+@Slf4j
+public class AnalyticsController implements Initializable {
+    @FXML
+    private JFXComboBox histoBox;
+
+    @FXML
+    private BarChart<?, ?> ServiceReqHisto;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        XYChart.Series set = new XYChart.Series<>();
+
+        set.getData().add(new XYChart.Data("Gift Delivery", 10));
+        set.getData().add(new XYChart.Data("Security", 20));
+        set.getData().add(new XYChart.Data("Maintenance", 14));
+        set.getData().add(new XYChart.Data("Internal Transportation", 23));
+        set.getData().add(new XYChart.Data("External Transportation", 36));
+        set.getData().add(new XYChart.Data("Medicine", 47));
+        set.getData().add(new XYChart.Data("Sanitation", 50));
+        set.getData().add(new XYChart.Data("IT", 13));
+        set.getData().add(new XYChart.Data("Interpreter", 25));
+        set.getData().add(new XYChart.Data("Reflection Room", 33));
+        set.getData().add(new XYChart.Data("On-Call Bed", 55));
+
+        ServiceReqHisto.getData().addAll(set);
+
+    }
+
+    @FXML
+    public void handleLive() {
+    }
+
+    @FXML
+    public void handleRefresh() {
+    }
 }
