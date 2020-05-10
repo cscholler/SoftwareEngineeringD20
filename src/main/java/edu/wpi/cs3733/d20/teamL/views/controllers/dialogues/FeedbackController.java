b145eed2fd3dd270042c0b7fd573e7d0afa7eb0e
Winnie Ly <wsly@wpi.edu>
2020-05-10 13:16:50 -0400

    Finished feedback

--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/dialogues/FeedbackController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/dialogues/FeedbackController.java
@@ -6,10 +6,12 @@ import com.jfoenix.controls.JFXTextField;
 import edu.wpi.cs3733.d20.teamL.services.db.DBConstants;
 import edu.wpi.cs3733.d20.teamL.services.db.IDatabaseService;
 import edu.wpi.cs3733.d20.teamL.services.db.SQLEntry;
+import edu.wpi.cs3733.d20.teamL.util.FXMLLoaderFactory;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.RadioButton;
+import javafx.scene.control.Label;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import javax.inject.Inject;
@@ -24,7 +26,7 @@ public class FeedbackController implements Initializable {
     private JFXTextField expectationsText, applicationText, otherText;
 
     @FXML
-    private JFXButton btnCancel, btnSubmit;
+    private JFXButton btnCancel;
 
     @FXML
     private JFXRadioButton expectationsYes, expectationsNo, easyYes, easyNo, navigateYes, navigateNo, nextYes, nextNo;
@@ -33,9 +35,14 @@ public class FeedbackController implements Initializable {
     private ToggleGroup expectationsGroup, easyGroup, navigateGroup, nextGroup;
 
     @FXML
+    private Label submissionLabel;
+
+    @FXML
     private Stage stage;
+
     @Inject
     private IDatabaseService db;
+    FXMLLoaderFactory loader = new FXMLLoaderFactory();
 
 
     @Override
@@ -50,8 +57,8 @@ public class FeedbackController implements Initializable {
         nextYes.setToggleGroup(nextGroup);
         nextNo.setToggleGroup(nextGroup);
 
-
     }
+
     @FXML
     void handleCancel() {
 
@@ -62,15 +69,46 @@ public class FeedbackController implements Initializable {
     @FXML
     void handleSubmit() {
 
+        boolean validFields = true;
+
         String expectations = expectationsText.getText();
         String application = applicationText.getText();
         String other = otherText.getText();
-        String choiceSelect = null;
 
-        int rows = db.executeUpdate((new SQLEntry(DBConstants.ADD_FEEDBACK,
-                new ArrayList<>(Arrays.asList(expectations, application, other)))));
 
-        if (rows == 1) {
+        if (expectations == null || expectations.equals("")) {
+            validFields = false;
+        } else {
+            validFields = true;
+        }
+        if (application == null || application.equals("")) {
+            validFields = false;
+        } else {
+            validFields = true;
+        }
+        if (other == null || other.equals("")) {
+            validFields = false;
+        } else {
+            validFields = true;
+        }
+
+        int rows = 0;
+
+        if (validFields) {
+            rows = db.executeUpdate((new SQLEntry(DBConstants.ADD_FEEDBACK,
+                    new ArrayList<>(Arrays.asList(expectations, application, other)))));
+        }
+
+        if (rows == 0) {
+
+            submissionLabel.setTextFill(Color.RED);
+            submissionLabel.setText("Please fill out all fields");
+
+        } else {
+
+            submissionLabel.setTextFill(Color.BLACK);
+            submissionLabel.setText("Thank you for your time!");
+
             expectationsText.setText("");
             applicationText.setText("");
             otherText.setText("");
@@ -83,6 +121,6 @@ public class FeedbackController implements Initializable {
             nextYes.setSelected(false);
             nextNo.setSelected(false);
         }
+        loader.showAndFade(submissionLabel);
     }
-
 }
