950fa5e862814fe8718f4fb4b5c4aee29a3f9115
Winnie Ly <wsly@wpi.edu>
2020-05-09 23:32:52 -0400

    Feedback table good to go functionality is there

--- src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/dialogues/FeedbackController.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/views/controllers/dialogues/FeedbackController.java
@@ -3,13 +3,19 @@ package edu.wpi.cs3733.d20.teamL.views.controllers.dialogues;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXRadioButton;
 import com.jfoenix.controls.JFXTextField;
+import edu.wpi.cs3733.d20.teamL.services.db.DBConstants;
+import edu.wpi.cs3733.d20.teamL.services.db.IDatabaseService;
+import edu.wpi.cs3733.d20.teamL.services.db.SQLEntry;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.stage.Stage;
 
+import javax.inject.Inject;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.ResourceBundle;
 
 public class FeedbackController implements Initializable {
@@ -28,6 +34,8 @@ public class FeedbackController implements Initializable {
 
     @FXML
     private Stage stage;
+    @Inject
+    private IDatabaseService db;
 
 
     @Override
@@ -58,6 +66,23 @@ public class FeedbackController implements Initializable {
         String application = applicationText.getText();
         String other = otherText.getText();
         String choiceSelect = null;
+
+        int rows = db.executeUpdate((new SQLEntry(DBConstants.ADD_FEEDBACK,
+                new ArrayList<>(Arrays.asList(expectations, application, other)))));
+
+        if (rows == 1) {
+            expectationsText.setText("");
+            applicationText.setText("");
+            otherText.setText("");
+            expectationsYes.setSelected(false);
+            expectationsNo.setSelected(false);
+            easyYes.setSelected(false);
+            easyNo.setSelected(false);
+            navigateYes.setSelected(false);
+            navigateNo.setSelected(false);
+            nextYes.setSelected(false);
+            nextNo.setSelected(false);
+        }
     }
 
 }
