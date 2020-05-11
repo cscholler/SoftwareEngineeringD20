a81f945132b43a043d906ec259bae9b8501c0543
Colin <cjscholler@wpi.edu>
2020-05-11 03:05:17 -0400

    kisok timeout checks and whatever

--- src/main/java/edu/wpi/cs3733/d20/teamL/services/db/DBConstants.java
+++ src/main/java/edu/wpi/cs3733/d20/teamL/services/db/DBConstants.java
@@ -241,6 +241,11 @@ public class DBConstants {
 					"FROM Nodes " +
 					"ORDER BY id";
 
+	public static final String GET_NODE =
+			"SELECT * " +
+					"FROM Nodes " +
+					"WHERE id = ?";
+
 	public static final String SELECT_ALL_EDGES =
 			"SELECT * " +
 					"FROM Edges " +
@@ -491,13 +496,9 @@ public class DBConstants {
 					"SET notes = ? " +
 					"WHERE id = ?";
 
-	public static final String UPDATE_KIOSK_LOCATION =
-			"UPDATE Kiosk_Settings " +
-					"SET node_id = ?";
-
 	public static final String UPDATE_KIOSK_TIMEOUTS =
 			"UPDATE Kiosk_Settings " +
-					"SET logout_timeout = ?, idle_cache_timeout = ?, force_cache_timeout = ?, screen_saver_timeout = ? " +
+					"SET node_id = ?, logout_timeout = ?, idle_cache_timeout = ?, force_cache_timeout = ?, screen_saver_timeout = ? " +
 					"WHERE id = ?";
 
 	public static final String UPDATE_SCREENING_QUESTIONS =
